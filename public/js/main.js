0b6587b96ecbb2e3d79c3427d1d67dba0bf14cbc
rosalyn0922 <rosalyn0922@gmail.com>
2018-11-10 15:27:36 -0500

    Fix Variable NewMap Global

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 let restaurants;
 let neighborhoods;
 let cuisines;
-let newMap;
+var newMap;
 const markers = [];
 
 /**
@@ -76,14 +76,14 @@ initMap = () => {
     center: [40.722216, -73.987501],
     zoom: 12,
     scrollWheelZoom: false,
-  });
+  });  
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1Ijoicm9zYWx5bjA5MjIiLCJhIjoiY2pvNTYwZmV4MDZtejNwczFtMmJ5cG9rbyJ9.4wsRPTtVcmVsknWN7JGGXA',
+    mapboxToken: 'pk.eyJ1Ijoicm9zYWx5bjA5MjIiLCJhIjoiY2pvYnd1eXZxMHY5ODNxbWs1ejVxMWpkOSJ9.U19jDfx0N4uMOVSKnYb9Gw',
     maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, '
-      + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '
-      + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
   }).addTo(newMap);
 
   updateRestaurants();
