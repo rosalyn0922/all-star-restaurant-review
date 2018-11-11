300af50cd2af2f10d1340761419c6fa07dae651a
rosalyn0922 <rosalyn0922@gmail.com>
2018-11-10 23:15:29 -0500

    Adjusted List and Filter to be responsive

--- public/js/main.js
+++ public/js/main.js
@@ -159,27 +159,33 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li')
 
+  const contentWrap = document.createElement('div')
+  contentWrap.className = 'content-wrap'
+
   const image = document.createElement('img')
   image.className = 'restaurant-img'
-  image.src = DBHelper.imageUrlForRestaurant(restaurant)
+  image.src = DBHelper.imageUrlForRestaurant(restaurant)  
   li.append(image)
 
+  
   const name = document.createElement('h1')
   name.innerHTML = restaurant.name
-  li.append(name)
-
+  contentWrap.append(name)
+  
   const neighborhood = document.createElement('p')
   neighborhood.innerHTML = restaurant.neighborhood
-  li.append(neighborhood)
+  contentWrap.append(neighborhood)
 
   const address = document.createElement('p')
   address.innerHTML = restaurant.address
-  li.append(address)
+  contentWrap.append(address)
 
   const more = document.createElement('a')
   more.innerHTML = 'View Details'
   more.href = DBHelper.urlForRestaurant(restaurant)
-  li.append(more)
+  contentWrap.append(more)
+
+  li.append(contentWrap)
 
   return li
 }
